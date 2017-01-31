ca0b7f6bba87060650b42018bc55a3419fcf0228
Mateusz Moneta <mateuszm@opera.com>
2017-01-31 21:25:08 +0100

    Hardcode query with author name, present first draft of parse method

--- linkedin_scraper/spiders/search.py
+++ linkedin_scraper/spiders/search.py
@@ -4,7 +4,13 @@ import scrapy
 class SearchSpider(scrapy.Spider):
     name = 'search'
     allowed_domains = ['linkedin.com']
-    start_urls = ['http://linkedin.com/']
+    start_urls = [
+        'https://www.linkedin.com/vsearch/f?type=people&keywords=MateuszMoneta']
 
     def parse(self, response):
-        pass
+        for search_result in response.css('li.mod.result.people'):
+            *first_name, last_name = search_result.css('b::text').extract()
+            yield {
+                'first_name': ' '.join(first_name),
+                'last_name': last_name,
+            }
