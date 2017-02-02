666fa8b3a1a850cbd678580740e97ca008dc9323
Mateusz Moneta <mateuszm@opera.com>
2017-02-02 08:31:56 +0100

    Handle empty and only country special cases in location parser.

--- linkedin_scraper/parsers/location.py
+++ linkedin_scraper/parsers/location.py
@@ -6,9 +6,13 @@ from linkedin_scraper.parsers.base import BaseParser
 class LocationParser(BaseParser):
     def parse(self, item: str) -> Tuple[str, str]:
         """
-        Parse LinkedIn location string into city and country.
+        Parse LinkedIn location string into city and country. Return
+        empty string for missing ones.
         :param item: location string
         :return: city, country
         """
-        city, *_, country = item.split(',')
+        try:
+            city, *_, country = item.split(',')
+        except ValueError:
+            return '', item
         return city.strip(), country.strip()
--- linkedin_scraper/parsers/tests/test_location.py
+++ linkedin_scraper/parsers/tests/test_location.py
@@ -9,9 +9,17 @@ def location_parser():
 
 
 def test_normal_location(location_parser):
-    assert ('Warszawa', 'Polska') == location_parser.parse(
-        'Warszawa, woj. mazowieckie, Polska')
+    assert ('Warszawa', 'Poland') == location_parser.parse(
+        'Warszawa, woj. mazowieckie, Poland')
 
 
 def test_location_contains_only_city_and_country(location_parser):
-    assert ('Warszawa', 'Polska') == location_parser.parse('Warszawa, Polska')
+    assert ('Warszawa', 'Poland') == location_parser.parse('Warszawa, Poland')
+
+
+def test_location_only_country(location_parser):
+    assert ('', 'Poland') == location_parser.parse('Poland')
+
+
+def test_location_empty(location_parser):
+    assert ('', '') == location_parser.parse('')
