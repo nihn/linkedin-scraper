b723afcb528ed992acdf3bed85e27ac8921bab10
Mateusz Moneta <mateuszm@opera.com>
2017-02-03 09:34:36 +0100

    Use professions_list.txt to deal with cases when users use customs employment strings.
    profession_list.txt are currently list of English profession names but can easily be replaced
    with some other language when needed.

--- linkedin_scraper/parsers/employment.py
+++ linkedin_scraper/parsers/employment.py
@@ -4,6 +4,10 @@ from linkedin_scraper.parsers.base import BaseParser
 
 
 class EmploymentParser(BaseParser):
+    def __init__(self):
+        self.professions_list = self.get_lines_from_datafile(
+            'professions_list.txt')
+
     def parse(self, item: str) -> Tuple[str, str]:
         """
         Parse LinkedIn employment string into position and company.
@@ -14,5 +18,20 @@ class EmploymentParser(BaseParser):
             # Easy case, standard LinkedIn format <position> at <company>
             return tuple(item.split(' at ', maxsplit=1))
 
-        # We don't know which is which so return whole item as a position
-        return item, ''
+        words = item.split()
+
+        for index, word in enumerate(reversed(item.split())):
+            normalized_word = word.strip(',.-').lower()
+
+            if normalized_word in self.professions_list:
+                founded_profession_index = len(words) - index
+                break
+        else:
+            # We don't know which is which so return whole item as a position
+            return item, ''
+
+        # We found profession name in employment string, everything
+        # after it is company name
+        return (' '.join(words[:founded_profession_index]).rstrip(',.-'),
+                ' '.join(words[founded_profession_index:]))
+
--- linkedin_scraper/parsers/tests/test_employment.py
+++ linkedin_scraper/parsers/tests/test_employment.py
@@ -5,7 +5,9 @@ from linkedin_scraper.parsers import EmploymentParser
 
 @fixture()
 def employment_parser():
-    return EmploymentParser()
+    parser = EmploymentParser()
+    parser.professions_list = []
+    return parser
 
 
 def test_regular_linkedin_format(employment_parser):
@@ -21,3 +23,17 @@ def test_unknown_format(employment_parser):
 def test_multiple_at_in_employment_str(employment_parser):
     assert ('Developer', 'Foo at Bar') == employment_parser.parse(
         'Developer at Foo at Bar')
+
+
+def test_position_found_in_profession_list(employment_parser):
+    employment_parser.professions_list = {'developer'}
+
+    assert ('Senior Python Developer', 'Foo Software') == \
+        employment_parser.parse('Senior Python Developer, Foo Software')
+
+
+def test_position_found_in_profession_list_without_company(employment_parser):
+    employment_parser.professions_list = {'developer'}
+
+    assert ('Senior Python Developer', '') == employment_parser.parse(
+        'Senior Python Developer')
